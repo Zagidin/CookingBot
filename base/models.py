c29fd1a5da8b240dffb4a6d816265a5e8e548e15
Zagidin <prap0235@gmail.com>
2024-12-12 00:24:25 +0300

    Zagidin

--- base/models.py
+++ base/models.py
@@ -11,7 +11,6 @@ from sqlalchemy import (
     create_engine
 )
 from sqlalchemy.orm import relationship
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
