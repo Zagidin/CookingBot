3fe178253ce38276dd52b3adbd4d31169119a725
Zagidin <prap0235@gmail.com>
2024-12-11 20:12:33 +0300

    Zagidin

--- handlers/admin/__init__.py
+++ handlers/admin/__init__.py
@@ -1,5 +1,4 @@
 from .start_admin import dp
-from .support import dp
 
 
 __all__ = ["dp"]
--- handlers/user/__init__.py
+++ handlers/user/__init__.py
@@ -1,6 +1,7 @@
 from .start import dp
 from .help import dp
 from .profile import dp
+from .support import dp
 from .navigation import dp
 
 
