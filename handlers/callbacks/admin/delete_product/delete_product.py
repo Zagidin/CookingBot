1e148fc64d1bae2c59675b3769d45bc7f90d19c9
Zagidin <prap0235@gmail.com>
2025-01-18 21:40:04 +0300

    Удаление продукта

--- handlers/callbacks/admin/delete_product/delete_product.py
+++ handlers/callbacks/admin/delete_product/delete_product.py
@@ -1,41 +1,39 @@
 from os import remove
 from bot.bot import dp
-from base.models import Product
 from sqlalchemy.orm import Session
 from base.base import SessionLocal
+from base.models import Product, Basket
 from sqlalchemy.exc import NoResultFound
 from aiogram.types import CallbackQuery
 
 
 @dp.callback_query_handler(lambda c: c.data.startswith("delete_product_in_category:"))
 async def delete_product(call: CallbackQuery):
-    """
-        🚩 Чтобы удалялся товар, нужно прописать проверку,
-            существует ли этот товар в корзине,
-                Если Истина, то удалить товар с Корзины и уже Потом с БД -> Продукта 🚩
-
-        🚩 УДАЛИТЬ СООБЩЕНИЕ В main.py ПОСЛЕ ИСПРАВЛЕНИЯ 🚩
-    """
 
     session: Session = SessionLocal()
 
     try:
-        product_id = int(call.data.split(":")[1])
+        id_product = int(call.data.split(":")[1])
 
         product = session.query(Product).filter(
-            Product.id == product_id
+            Product.id == id_product
         ).one_or_none()
 
         remove(product.photo)
 
         if product:
+            session.query(Basket).filter(
+                Basket.product_id == id_product
+            ).delete()
+
             session.delete(product)
             session.commit()
+
             await call.message.answer("✅")
             await call.message.answer(f"Товар успешно удалён ✅")
 
         else:
-            await call.message.answer(f"Товар с ID {product_id} не найден.")
+            await call.message.answer(f"Товар с ID {id_product} не найден.")
     except NoResultFound:
         await call.message.answer(f"Товар не найден.")
     except Exception as e:
